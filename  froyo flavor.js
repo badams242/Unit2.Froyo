57aa5b17d255fb7dbb0f9c070159d62dc43db5e8
Brandon <146400057+badams242@users.noreply.github.com>
2023-11-16 12:33:32 -0500

    Created an oject to store the count of each flavor and count of the occurrences of each flavor

---  froyo flavor.js
+++  froyo flavor.js
@@ -1,8 +1,8 @@
 const userInput = prompt('Please enter your favorite froyo flavor: vanilla, strawberry, or coffee')
 const flavorsArray = userInput.split(['vanilla', 'strawberry', 'coffee'])
-const flavorCount = flavorsArray.reduce((flavorCount, flavor) => {
-    flavorCount[flavor] = (flavorCount[flavor] || 0) + 1
-    return flavorCount
-}, {})
-console.table(flavorCount)
+const flavorCount= flavorsArray.length
+for ( const flavor of flavorsArray) {
+    flavorCount[flavor] = (flavorCount[flavor] || 0) + 1;
+}
+console.table(flavorCount);
 
