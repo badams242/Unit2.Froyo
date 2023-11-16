4a7f9eec3c0a461864845a3ee60216a42066dbf2
Brandon <146400057+badams242@users.noreply.github.com>
2023-11-16 09:49:16 -0500

    created froyo flavor.html and froyo flavor.js  Prompt the list of comma-separated froyo flavors  Used the split() method to convert the string to an array  Created an abject to store the flavors and number for certain flavors  count the occurrences of each flavor  log the result in table format

---
+++  froyo flavor.js
@@ -0,0 +1,13 @@
+const userInput = prompt(vanilla, vanilla, vanilla, strawberry, coffee, coffee)
+const flavorsArray = userInput.split(['vanilla', 'strawberry', 'coffee'],) 
+const flavorCount = {vanilla: 1, strawberry: 1, coffee: 2}
+const flavorCount = flavorsArray.reduce(function (flavorCount, flavor) {
+    if (flavorCount[flavor]) {
+        flavorCount[flavor]++
+    } else {
+        flavorCount[flavor] = 1
+    }
+    return flavorCount
+}, {})
+console.table(flavorCount)  
+
