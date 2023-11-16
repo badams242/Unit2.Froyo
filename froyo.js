0a2b78c278f65b835cc250a214e74a9f4564e94d
Brandon <146400057+badams242@users.noreply.github.com>
2023-11-16 15:05:10 -0500

    formatted code

--- froyo.js
+++ froyo.js
@@ -1,16 +1,15 @@
 function trackfroyoflavors() {
-const userInput = prompt("Pick a flavor of froyo, vanilla, strawberry,coffee")
+    const userInput = prompt("Pick a flavor of froyo, vanilla, strawberry,coffee")
 
-const flavorsArray = userInput.split(',');
+    const flavorsArray = userInput.split(',');
 
-const flavorCount ={
-    vanilla: 3,
-    strawberry: 1,
-    coffee: 2
-};
+    const flavorCount = {
+        vanilla: 3,
+        strawberry: 1,
+        coffee: 2
+    };
 
-for (const flavor of flavorsArray)
-{flavorCount[flavor] = (flavorCount[flavor]|| 0) + 1;}
-console.table(flavorCount);
+    for (const flavor of flavorsArray) { flavorCount[flavor] = (flavorCount[flavor] || 0) + 1; }
+    console.table(flavorCount);
 }
 trackfroyoflavors();
