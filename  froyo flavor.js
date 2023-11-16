eb6ea2631958c9dd4713838a6a450de38f66e37f
Brandon <146400057+badams242@users.noreply.github.com>
2023-11-16 11:40:04 -0500

    delete if andelse stament made new statement with for loop

---  froyo flavor.js
+++  froyo flavor.js
@@ -1,12 +1,9 @@
 const userInput = prompt(vanilla, vanilla, vanilla, strawberry, coffee, coffee)
 const flavorsArray = userInput.split(['vanilla', 'strawberry', 'coffee'],)
 const flavorCount = { vanilla: 3, strawberry: 1, coffee: 2 }
-const flavorCount = flavorsArray.reduce(function (flavorCount, flavor) {
-    if (flavorCount[flavor]) {
-        flavorCount[flavor]++
-    } else {
-        flavorCount[flavor] = 1
-    }
+for (const flavor of flavorsArray) { flavorCount[flavor] = (flavorCount[flavor] || 0) + 1 }
+const flavorCount = flavorsArray.reduce((flavorCount, flavor) => {
+    flavorCount[flavor] = (flavorCount[flavor] || 0) + 1
     return flavorCount
 }, {})
 console.table(flavorCount)
