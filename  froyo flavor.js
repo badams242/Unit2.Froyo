bf942e7948eb5c055d5ae38d9c06fa8a490f2d99
Brandon <146400057+badams242@users.noreply.github.com>
2023-11-16 11:43:58 -0500

    no changes

---  froyo flavor.js
+++  froyo flavor.js
@@ -1,5 +1,5 @@
 const userInput = prompt(vanilla, vanilla, vanilla, strawberry, coffee, coffee)
-const flavorsArray = userInput.split(['vanilla', 'strawberry', 'coffee'],)
+const flavorsArray = userInput.split['vanilla', 'strawberry', 'coffee']
 const flavorCount = { vanilla: 3, strawberry: 1, coffee: 2 }
 for (const flavor of flavorsArray) { flavorCount[flavor] = (flavorCount[flavor] || 0) + 1 }
 const flavorCount = flavorsArray.reduce((flavorCount, flavor) => {
