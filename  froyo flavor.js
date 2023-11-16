af768d3ee26a2f43e4b68bf2cb6829b6695aec23
Brandon <146400057+badams242@users.noreply.github.com>
2023-11-16 14:07:56 -0500

    promot the user list of froyo flavors split input string into an array of flavors create an object to store the count of each flavor count the occurrences of each flavor log the result in table format

---  froyo flavor.js
+++
@@ -1,8 +0,0 @@
-const userInput = prompt('Please enter your favorite froyo flavor: vanilla, strawberry, or coffee')
-const flavorsArray = userInput.split(['vanilla', 'strawberry', 'coffee'])
-const flavorCount= flavorsArray.length
-for ( const flavor of flavorsArray) {
-    flavorCount[flavor] = (flavorCount[flavor] || 0) + 1;
-}
-console.table(flavorCount);
-
