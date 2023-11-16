911f4e820914917c600774c01ef0d7312cb97c86
Brandon <146400057+badams242@users.noreply.github.com>
2023-11-16 09:57:00 -0500

    chnaged number for flavor count

---  froyo flavor.js
+++  froyo flavor.js
@@ -1,6 +1,6 @@
 const userInput = prompt(vanilla, vanilla, vanilla, strawberry, coffee, coffee)
 const flavorsArray = userInput.split(['vanilla', 'strawberry', 'coffee'],) 
-const flavorCount = {vanilla: 1, strawberry: 1, coffee: 2}
+const flavorCount = {vanilla: 3, strawberry: 1, coffee: 2}
 const flavorCount = flavorsArray.reduce(function (flavorCount, flavor) {
     if (flavorCount[flavor]) {
         flavorCount[flavor]++
